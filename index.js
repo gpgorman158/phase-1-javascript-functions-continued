8ccff1dc96083b6d99e96eaafe86a84d6e077285
Gerald Gorman <gpgorman158@gmail.com>
2022-02-01 10:50:00 -0500

    commit tree b78e737ecd4ab5101040641afd8312c4b65a6d8f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity2 = 'go to the office') {
+    return `This Monday, I will ${activity2}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective (flare = '*') {
+    return function (adjective = 'special'){
+        return `You are ${flare}${adjective}${flare}!`;
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
